df608607c319fad15ba26dd8cdc11928cf8a28a2
Rocasazza11 <104740389+Rocasazza11@users.noreply.github.com>
2022-06-29 22:00:23 -0500

    Add files via upload

---
+++ Python/Tercer parcial/Final  Game.py
@@ -0,0 +1,506 @@
+import random 
+
+print('MODE CARRERA')
+print("""___
+      .:::---:::.
+    .'--:     :--'.
+   /.'   \   /   `.\
+  
+  | /'._ /:::\ _.'\ |
+  |/    |:::::|    \|
+  |:\ .''-:::-''. /:|
+   \:|    `|`    |:/
+    '.'._.:::._.'.'
+     '-:::::::-'""")
+
+input()
+print("Welcome to mode carerra")
+input()
+print("This game is about taking decisions about a football career.")
+input()
+print(
+    "In this game you are going to read and play the career of Alex Ramirez he is a bring young man with 19 years old")
+        
+
+input()
+print(
+    "he is Mexican who was trained in the america  youth academy and in the 21-22 season he rose to the first team,"
+)
+input()
+print("Alex ramirez's position is forward.")
+input()
+print("His debut will be against Santos Laguna")
+input()
+print("He was in the bench until")
+input()
+print("He enters in the 65th minute")
+input()
+print("Henry Martin leaves.")
+input()
+print(
+    "The score is 2-1, in the final minutes of the match Córdoba sends a filtered pass to Ramírez "
+)
+input()
+print("That leaves him alone againts the goalkeeper....")
+print("""    -   \O                                     ,  .-.___
+  -     /\                                   O/  /xx\XXX\
+ -
+     __/\ `                                  /\  |xx|XXX|
+    `    \, ()                              ` << |xx|XXX|
+jgs^^^^^^^^`^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^""")
+
+input()
+print("WHERE TO SHOOT?")
+
+print("a)Left")
+print("b)Right")
+a = input("-->")
+
+if (a == "a"):
+    print("He makes a goal!!")
+    input()
+    print("The game ends 3-1 ")
+    input()
+    print("Everyone celebrates!")
+    input()
+    print("-----------------------------")
+    input()
+    print("In the next game, he starts against Pachuca....")
+    input()
+    print("and wins 2-0 favor of America")
+
+else:
+    print("The goalkeeper stops it ")
+    input()
+    print("The game ends, since it was the last play.")
+    input()
+    print("------------------------------")
+    input()
+    print("In the next game, he is a bench")
+    input()
+    print("He never plays, his team played against Pachuca")
+    input()
+    print("They win, 2-0, favor America")
+    input()
+
+print("Later....")
+input()
+input()
+print("---------------------------------")
+input()
+print("One friday, Alex was invited to a party,")
+input()
+print("But he had an important game against Chivas, the next day")
+input()
+print("the Mexican soccer classic on Sunday")
+input()
+
+while True:
+    print("What will alex do?")
+    print("a)Stay home")
+    print("b)Go to the party")
+    b = input("-->")
+    
+    if (b == "a"):
+        print("Alex decides to stay home, have dinner and sleep early watching tv series")
+        input()
+        input()
+        print("-------------------------")
+        input()
+        print("THE DAY OF THE MATCH AGAINST THE CHIVAS ARRIVES")
+        input()
+        print("Alex is ready and fit for the match, fully fit")
+        input()
+
+      
+        input()
+        print("The game starts")
+        input()
+        print("After 10 minutes Alex scores a volley goal")
+        input()
+        print("he plays the game very well and its the best of the game")
+        input()
+        print("The game ends 3-2")
+        input()
+        print("The America wins with two goals from Alex") 
+        input()
+        print("they go first in the league")
+        input()
+        print("""       ____
+       ( () )
+        \  /
+         ||
+         ||
+        [__]
+       /)  (\
+      (( () ))
+       \\__//
+        `..'
+         ||
+         ||    lf
+        //\\__
+     _ ((  `--'
+    (_) \)""")
+
+    elif(b=="b"):
+      input()
+      print("Alex attends to the party and drinks too much")
+      input()
+      print("He even stayed until 3 am at the party.")
+      input()
+      input()
+      print("---------------------------")
+      input()
+      print("THE DAY OF THE MATCH AGAINST CHIVAS ARRIVED ")
+      input()
+      print("Alex was feeling bad all the weekend thanks to that party,")
+      input()
+      print("he still wasnt fit for the game")
+      input()
+      print("He really wanted to vomit and he had a headache")
+      input()
+      print("The game started, but alex could not be in the game phisycally")
+      input()
+      print("All his body was hurting, and he was dizzy")
+      input()
+      print("He asked for a change")
+      input()
+      print("He could only play for the first 25 minutes.")
+      input()
+      print("The match ends 2-2")
+      input()
+      print("It all depends on the penals")
+      input()
+      print("The team coach selects the players that are going to play")
+      input()
+      print("Is Alex going to play with his conditions?")
+      input()
+      
+      lista1=[1,2]
+      eleccion1=random.choice(lista1)
+      jugador=eleccion1
+      
+      while True:
+        print("Choose:")
+        print("a)1")
+        print("b)2")
+        c=input("--->")
+        
+        if (c==jugador):
+          print("The coach has decided")
+          input()
+          print("Alex is going to play last")
+          input()
+          print("Even if Alex is feeling bad")
+          input()
+          print("Because he is the best player scoring goals")
+          input()
+          print("The moment arrives..")
+          input()
+          print("Alex's team starts")
+          input()
+          
+          lista2=[1,2]
+          lccion2=random.choice(lista2)
+          gol1=lccion2
+          
+          print("Choose:")
+          print("a)1")
+          print("b)2")
+          d=input("-->")
+          
+          if (d==gol1):
+            print("Alex makes a goal!!!")
+            input()
+            print("Leaving the game 3-2")
+            input()
+            print("CONGRATULATIONS!!")
+            input()
+            print("His team has won thanks to your good decisions")
+          
+            input()
+            break
+        
+          else:
+            print("The last goal has been scored")
+            input()
+            print("Leaving the game 2-3")
+            input()
+            print("Alex goal failed")
+            input()
+          
+            print("Alex's team has lost")
+            input()
+            print("Everybody is looking meaningfully to him")
+            input()
+            print("It seems like everybody hates him now...")
+            input()
+            input()
+            print("Im sorry..")
+            input()
+            print("Have better decision next time")
+            input()
+            print("""⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠀⠀⠀⣶⡆⠀⣰⣿⠇⣾⡿⠛⠉⠁
+⠀⣠⣴⠾⠿⠿⠀⢀⣾⣿⣆⣀⣸⣿⣷⣾⣿⡿⢸⣿⠟⢓⠀⠀
+⣴⡟⠁⣀⣠⣤⠀⣼⣿⠾⣿⣻⣿⠃⠙⢫⣿⠃⣿⡿⠟⠛⠁⠀
+⢿⣝⣻⣿⡿⠋⠾⠟⠁⠀⠹⠟⠛⠀⠀⠈⠉⠀⠉⠀⠀⠀⠀⠀
+⠀⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⣀⢀⣠⣤⣴⣤⣄⠀
+⠀⠀⠀⠀⣀⣤⣤⢶⣤⠀⠀⢀⣴⢃⣿⠟⠋⢹⣿⣣⣴⡿⠋⠀
+⠀⠀⣰⣾⠟⠉⣿⡜⣿⡆⣴⡿⠁⣼⡿⠛⢃⣾⡿⠋⢻⣇⠀⠀
+⠀⠐⣿⡁⢀⣠⣿⡇⢹⣿⡿⠁⢠⣿⠷⠟⠻⠟⠀⠀⠈⠛⠀⠀
+⠀⠀⠙⠻⠿⠟⠋⠀⠀⠙⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀""")
+            
+            input()
+            continue
+            
+        else:
+          print("The coach has decided...")
+          input()
+          print("Alex is not going to play now")
+          input()
+          print("All of the crowd is nervous")
+          input()
+          print("Is Alex's team going to win?")
+          e=int(input("Choose 1 or 2:"))
+          input()
+          
+          if(e==1):
+            print("The last goal has been done")
+            input()
+            print("Leaving the game 2-3")
+            input()
+            print("Alex's team has lost")
+            input()
+            input()
+            print("Have better decision next time")
+            input()
+            
+            print("""⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠀⠀⠀⣶⡆⠀⣰⣿⠇⣾⡿⠛⠉⠁
+⠀⣠⣴⠾⠿⠿⠀⢀⣾⣿⣆⣀⣸⣿⣷⣾⣿⡿⢸⣿⠟⢓⠀⠀
+⣴⡟⠁⣀⣠⣤⠀⣼⣿⠾⣿⣻⣿⠃⠙⢫⣿⠃⣿⡿⠟⠛⠁⠀
+⢿⣝⣻⣿⡿⠋⠾⠟⠁⠀⠹⠟⠛⠀⠀⠈⠉⠀⠉⠀⠀⠀⠀⠀
+⠀⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⣀⢀⣠⣤⣴⣤⣄⠀
+⠀⠀⠀⠀⣀⣤⣤⢶⣤⠀⠀⢀⣴⢃⣿⠟⠋⢹⣿⣣⣴⡿⠋⠀
+⠀⠀⣰⣾⠟⠉⣿⡜⣿⡆⣴⡿⠁⣼⡿⠛⢃⣾⡿⠋⢻⣇⠀⠀
+⠀⠐⣿⡁⢀⣠⣿⡇⢹⣿⡿⠁⢠⣿⠷⠟⠻⠟⠀⠀⠈⠛⠀⠀
+⠀⠀⠙⠻⠿⠟⠋⠀⠀⠙⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀""")
+            input()
+            continue
+            
+          if (e==2):
+            print("The last goal has been made")
+            input()      
+            print("ALEX SCORED A GOAL!")
+            input()
+            print("Leaving the game 3-2")
+            input()
+            print("CONGRATULATIONS!!")
+            input()
+            print("His team has won thanks to your good decisions")
+            input()
+            break
+    print("---x------------------------")
+    input()
+    print("Later:")
+    input()
+    print("When Alex is going to leave, someone asks to talk to him")
+    input()
+    print("It was the coach from a more successful team")
+    input()
+    print("Are you interested in listening him?")
+    input()
+    print("a)No")
+    print("b)Yes")
+    f=input("-->")
+
+    
+  
+    if(f=="a"):
+      input()
+      print("Alex gently says he needs to go")
+      input()
+      print("Which excuse is he using?")
+      input()
+      print("a)My coach is calling me")
+      print("b)I need to go to pee")
+      f=input("--->")
+      if f=="a":
+        print("Alex uses his excuse")
+        input()
+        print("He tells him that is very important")
+        input()
+        print("And that he will love to hear what he says")
+        input()
+        print("But Alex leaves anyways")
+        input()
+        print("And goes with his coach")
+     
+      if f=="b":
+        print("Alex says he needs to pee")
+        input()
+        print("The man says its imortat")
+        input()
+        print("But Alex leaves anyways")
+        input()
+        print("Heading to the bathroom but then")
+        input()
+        print("Changing route to his coach")
+      
+      
+      input()
+      print("When he tells to his coach what happened..")
+      input()
+      print("He says that it was very important")
+      input()
+      print("You have lost an important option")
+      input()
+      print("Alex feels sad about it")
+      input()
+      print("He doesnt play well on his next game")
+      input()
+      print("Now he is in danger of being expulsed of the team")
+      input()
+      print("Wrong decisions")
+      input()
+      input()
+      print("""⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠀⠀⠀⣶⡆⠀⣰⣿⠇⣾⡿⠛⠉⠁
+⠀⣠⣴⠾⠿⠿⠀⢀⣾⣿⣆⣀⣸⣿⣷⣾⣿⡿⢸⣿⠟⢓⠀⠀
+⣴⡟⠁⣀⣠⣤⠀⣼⣿⠾⣿⣻⣿⠃⠙⢫⣿⠃⣿⡿⠟⠛⠁⠀
+⢿⣝⣻⣿⡿⠋⠾⠟⠁⠀⠹⠟⠛⠀⠀⠈⠉⠀⠉⠀⠀⠀⠀⠀
+⠀⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⣀⢀⣠⣤⣴⣤⣄⠀
+⠀⠀⠀⠀⣀⣤⣤⢶⣤⠀⠀⢀⣴⢃⣿⠟⠋⢹⣿⣣⣴⡿⠋⠀
+⠀⠀⣰⣾⠟⠉⣿⡜⣿⡆⣴⡿⠁⣼⡿⠛⢃⣾⡿⠋⢻⣇⠀⠀
+⠀⠐⣿⡁⢀⣠⣿⡇⢹⣿⡿⠁⢠⣿⠷⠟⠻⠟⠀⠀⠈⠛⠀⠀
+⠀⠀⠙⠻⠿⠟⠋⠀⠀⠙⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀""")
+
+      
+      continue
+
+  
+    if (f=="b"):
+      print("Alex listens carefully to the men")
+      input()
+      print("")
+      print("He asks him if he wants to join his team ")
+      input()
+      print("With a condition...")
+      input()
+      input()
+      print("Leave all your life in this country and go with him to another")
+      input()
+      print("That includes his family, his girlfriend/wife, his son, his pets..")
+  
+      input()
+      print("And his team members")
+      input()
+      print("But he will get more money and more fame") 
+      input()
+      print("Is Alex going accept the offer?")
+      print("Yes or no?") 
+      g=input("-->")
+
+      
+      if(g=="yes"):
+        input()
+        print("He says yes")
+        input()
+        print("In one week, he leaves all of his family and others")
+        input()
+        print("Everybody of the new team is kind with him")
+        input()
+        print("They actually make a welcome party for you")
+        input()
+        print("A day before his first match")
+        input()
+        print("Are you going to accept?")
+        print("a)No, he is not making the same error")
+        
+        print("b)Yes, why not? We are alive right now")
+       
+        h=input("--->")
+
+        
+        
+        if(h=="a"):
+          input()
+          print("Alex declines the invitation")
+          input()
+          print("The next day, his team wins")
+          input()
+          print("But now everybody is a little distant...")
+          input()
+          print("Since Alex didnt go to his own welcoming party")
+          input()
+          print("Alex has a good carreer in that team")
+          input()
+          print("But he does not have a good relation with his teammates")
+          input()
+          print("Now he is a little famous, and has a good life")
+          input()
+          print("He decides to return to his old team")
+          input()
+          print("He is happy now")
+          input()
+          print("with his family, and old friends")
+          input()
+          input()
+          print("YOU REACHED: ENDING NUMBER ONE!!")
+          input()
+          print("If you wanto to experience the other ending, try playig this game again")
+          input()
+
+          
+          break
+
+          
+        
+        if(h=="b"):
+          input()
+
+          print("He says yes")
+          input()
+          print("Why not? You only live life once")
+          input()
+          
+          print("In the party, he makes himself friends with all the teammates")
+          input()
+          print("He has a good time")
+          input()
+          print("He does not drink too much ")
+          
+          
+          input()
+          print("and he even leaves the party early")
+          input()
+          print("--------------------------------")
+          input()
+          print("The next day, at the game...")
+          input()
+         
+          print("He played well, he made 2 of the 4 goals that the team made")
+          input()
+          print("Now he is a very famous player")
+          input()
+          print("And a good one")
+          input()
+          print("He earns a lot of money")
+          input()
+          print("His family moved with him ")
+          input()
+          print("And he has the best soccer carreer he could have had")
+          input()
+          input()
+          
+          print("YOU HAVE REACHED ENDING NUMBER 2")
+         
+          input()
+          print("If you wanto to experience the other ending, try playing this game again")
+          input()
+          input()
+          
+          
+          break
+          
